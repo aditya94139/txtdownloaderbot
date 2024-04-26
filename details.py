fecc474f66b1590b07ad4738fea4b03930fcaf11
aditya94139 <152467027+aditya94139@users.noreply.github.com>
2024-04-26 10:31:54 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 18516364
-api_hash = "2e90970ea9e3ad55a3d7566754274fae"
-bot_token = "2057624136:AAGqDUgyUmtNZjeaO-ySc-YYJhTomhS1hmA"
+api_id = 23163380
+api_hash = "2dca155e786c7db2d295e5b4ab10783b"
+bot_token = "7175346598:AAGU_GZylGvyKTFj0QytKZiylhV0B_97IiI"
 
